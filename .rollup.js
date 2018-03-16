e2b41433e6423f4ffb6afd4a1baa515cdd6600af
Jonathan Neal <jonathantneal@hotmail.com>
2018-03-16 10:24:57 -0400

    Distribute cjs,es bundles

--- .rollup.js
+++ .rollup.js
@@ -2,7 +2,10 @@ import babel from 'rollup-plugin-babel';
 
 export default {
 	input: 'index.js',
-	output: { file: 'index.bundle.js', format: 'cjs' },
+	output: [
+		{ file: 'index.cjs.js', format: 'cjs' },
+		{ file: 'index.es.js', format: 'es' }
+	],
 	plugins: [
 		babel({
 			presets: [
